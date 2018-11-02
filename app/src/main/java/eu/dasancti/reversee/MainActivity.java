354193a849e817f33b5924e92f9bdea07810a63e
Tibor Dancs <tdancs@redhat.com>
2018-11-02 15:19:08 +0100

    Added an option to share image from URL

--- app/src/main/java/eu/dasancti/reversee/MainActivity.java
+++ app/src/main/java/eu/dasancti/reversee/MainActivity.java
@@ -31,6 +31,7 @@ import java.util.concurrent.atomic.AtomicReference;
 public class MainActivity extends AppCompatActivity {
 
     private static final String GOOGLE_REVERSE_IMAGE_SEARCH_URL = "https://www.google.com/searchbyimage/upload";
+    private static final String GOOGLE_REVERSE_IMAGE_SEARCH_URL_BY_URL = "https://www.google.com/searchbyimage?&image_url=";
     private static final String FAKE_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11";
     private static final String API_SCH_KEY = "sch";
     private static final String API_SCH_VALUE = "sch";
@@ -49,38 +50,67 @@ public class MainActivity extends AppCompatActivity {
         progressStatus = findViewById(R.id.progressStatus);
 
         Intent intent = getIntent();
+        if (intent == null) {
+            Toast.makeText(MainActivity.this, "No intent sent to application, closing.", Toast.LENGTH_SHORT).show();
+            this.finish();
+            return;
+        }
         String action = intent.getAction();
         if (action == null) {
             Toast.makeText(MainActivity.this, "No action sent to application, closing.", Toast.LENGTH_SHORT).show();
             this.finish();
             return;
         }
-        if (isPermissionGranted()) {
-            if (action.equals(Intent.ACTION_SEND)) {
-                progressStatus.setText(getString(R.string.progress_status_recieved_intent));
-                try {
-                    handleImageSearch(intent.getParcelableExtra(Intent.EXTRA_STREAM));
-                } catch (FileNotFoundException e) {
-                    String err = "Failed to handle Share image intent:"+e.getMessage();
-                    Log.e("INTENT_HANDLE",err);
-                    Toast.makeText(getApplicationContext(), err, Toast.LENGTH_SHORT).show();
+        if (action.equals(Intent.ACTION_SEND)) {
+            if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String intentExtraText = intent.getParcelableExtra(Intent.EXTRA_TEXT).toString();
+                progressStatus.setText(getString(R.string.progress_status_parsing_link));
+                if (intentExtraText.endsWith(".jpg") ||
+                    intentExtraText.endsWith(".png") ||
+                    intentExtraText.endsWith(".gif") ||
+                    intentExtraText.endsWith(".webp")) {
+                    progressStatus.setText(getString(R.string.progress_status_opening_from_url));
+                    Toast.makeText(MainActivity.this, getString(R.string.progress_status_opening_from_url), Toast.LENGTH_SHORT).show();
+                    Intent searchIntent = new Intent();
+                    searchIntent.setAction(Intent.ACTION_VIEW);
+                    searchIntent.setData(Uri.parse(GOOGLE_REVERSE_IMAGE_SEARCH_URL_BY_URL.concat(intentExtraText)));
+                    startActivity(searchIntent);
+                    MainActivity.this.finish();
+                } else {
+                    progressStatus.setText(getString(R.string.progress_status_unsupported_format));
+                    Toast.makeText(MainActivity.this, getString(R.string.progress_status_unsupported_format), Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+            } else if (intent.hasExtra(Intent.EXTRA_STREAM)) {
+                if (isPermissionGranted()) {
+                    progressStatus.setText(getString(R.string.progress_status_recieved_intent));
+                    try {
+                        handleImageSearch(intent.getParcelableExtra(Intent.EXTRA_STREAM));
+                    } catch (FileNotFoundException e) {
+                        String err = "Failed to handle Share image intent:" + e.getMessage();
+                        Log.e("INTENT_HANDLE", err);
+                        Toast.makeText(getApplicationContext(), err, Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Read storage permission required.");
+                    builder.setMessage("This application requires READ_EXTERNAL_STORAGE permission in order to function properly.");
+                    builder.setPositiveButton("Grant permission", (dialog, which) -> {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_EXTERNAL_STORAGE_STATE);
+                        if (action.equals(Intent.ACTION_SEND)) {
+                            requestPermissionsFallbackUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                        }
+                    });
+                    builder.setNegativeButton("Deny permission", ((dialog, which) -> {
+                        Toast.makeText(MainActivity.this, "Permission not granted, closing.", Toast.LENGTH_SHORT).show();
+                        this.finish();
+                    }));
+                    builder.create().show();
                 }
             }
         } else {
-            android.support.v7.app.AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Read storage permission required.");
-            builder.setMessage("This application requires READ_EXTERNAL_STORAGE permission in order to function properly.");
-            builder.setPositiveButton("Grant permission", (dialog, which) -> {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_EXTERNAL_STORAGE_STATE);
-                if (action.equals(Intent.ACTION_SEND)) {
-                    requestPermissionsFallbackUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-                }
-            });
-            builder.setNegativeButton("Deny permission", ((dialog, which) -> {
-                Toast.makeText(MainActivity.this, "Permission not granted, closing.", Toast.LENGTH_SHORT).show();
-                this.finish();
-            }));
-            builder.create().show();
+            Toast.makeText(MainActivity.this, "Unhandled action sent to application, closing.", Toast.LENGTH_SHORT).show();
+            this.finish();
         }
     }
 
@@ -156,7 +186,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        // TODO: Display spinner until requestHandle.isFinished() is true
     }
 
     private boolean isPermissionGranted() {
